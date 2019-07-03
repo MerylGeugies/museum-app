45f64096620051793d4fa2b4349208e1a416739f
MerylGeugies <mgeugies@gmail.com>
2019-07-03 18:25:30 +0200

    Meryl made display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -3,21 +3,22 @@ function submitComment() {
 //gather data
     const inputField = document.getElementById('name')
     const name = inputField.value
-    console.log(name)
     const textArea = document.getElementById("msg")
     const msg = textArea.value
-    console.log(msg)
 //create the elements you need
-    const comment = document.createElement(section)
-    const h3 = document.createElement(h3)
-    const p = document.createElement(p)
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
 //adjust elements we created
     h3.innerHTML = `${name} said:`
     p.innerHTML = msg
     comment.classList.add('comment')
     comment.appendChild(h3)
     comment.appendChild(p) 
-    
-    console.log(comment)
-
+//display the elements to the page
+    const commentSection = document.getElementById('comments')
+    commentSection.appendChild(comment)
+ //reset form values
+    inputField.value = null
+    textArea.value = null   
 }
